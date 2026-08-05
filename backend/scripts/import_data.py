da3e818c3d5009091bd38f2201bf86cdb5d50c2e
ShreyaC8 <shreya.chawla218@outlook.com>
2026-08-05 12:07:49 +0100

    data import improved with use of exceptions

--- backend/scripts/import_data.py
+++ backend/scripts/import_data.py
@@ -1,43 +1,55 @@
 import pandas as pd
+import sqlalchemy.exc
 
 from app.database import Base, engine, SessionLocal
 from app.models import Track
+from sqlalchemy.exc import SQLAlchemyError
 
 def load_dataset(dataset):
-    data = pd.read_csv(dataset)
-    return data
+    try:
+        return pd.read_csv(dataset)
+    except FileNotFoundError:
+        print("File not found")
 
 def import_tracks(dataframe, database):
+    import_amt = 0
     for row in dataframe.itertuples():
-        current_track = Track(track_id = row.track_id,
-                            track_name = row.track_name,
-                            artists = row.artists,
-                            album_name = row.album_name,
-                            track_genre = row.track_genre,
-                            popularity = row.popularity,
-                            duration_ms = row.duration_ms,
-                            explicit = row.explicit,
-                            energy = row.energy,
-                            danceability = row.danceability,
-                            valence = row.valence,
-                            key = row.key,
-                            loudness = row.loudness,
-                            mode = row.mode,
-                            speechiness = row.speechiness,
-                            acousticness = row.acousticness,
-                            instrumentalness = row.instrumentalness,
-                            liveness = row.liveness,
-                            tempo = row.tempo,
-                            time_signature = row.time_signature
-                            )
+        current_track = Track(
+            track_id = row.track_id,
+            track_name = row.track_name,
+            artists = row.artists,
+            album_name = row.album_name,
+            track_genre = row.track_genre,
+            popularity = row.popularity,
+            duration_ms = row.duration_ms,
+            explicit = row.explicit,
+            energy = row.energy,
+            danceability = row.danceability,
+            valence = row.valence,
+            key = row.key,
+            loudness = row.loudness,
+            mode = row.mode,
+            speechiness = row.speechiness,
+            acousticness = row.acousticness,
+            instrumentalness = row.instrumentalness,
+            liveness = row.liveness,
+            tempo = row.tempo,
+            time_signature = row.time_signature)
         database.add(current_track)
-    database.commit()
-    return len(dataframe)
+        import_amt += 1
+    try:
+        database.commit()
+        return import_amt
+    except SQLAlchemyError:
+        database.rollback()
+        print("Import failed")
+        return 0
 
 def main():
     Base.metadata.create_all(engine)
     db = SessionLocal()
     df = load_dataset("data/spotifydatabase.csv")
-    num_of_imports = import_tracks(df, db)
+    if df != None:
+        num_of_imports = import_tracks(df, db)
+        print(f"Loaded {num_of_imports} songs")
     db.close()
-    print(f"Loaded {num_of_imports} songs")
